ea739658a2f78762e455acfb9d6df5c6518fa2f6
Mark_Maistrenko <revanmayfall@gmail.com>
2023-01-02 04:21:50 +0400

    update -minAgeRestriction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { isNull } from "util";
 import { brotliDecompress } from "zlib";
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,7 +15,7 @@ type TypeVidios = {
   minAgeRestriction: number | null;
   createdAt: string;
   publicationDate: string;
-  availableResolutions: Array<string | null>;
+  availableResolutions: Array<string>;
 };
 
 let bd: { videos: TypeVidios[] } = { videos: [] };
@@ -23,188 +24,230 @@ app.get("/videos", (req: Request, res: Response) => {
   res.status(200).json(bd.videos);
 });
 
-app.post("/videos", (req: Request, res: Response) => {
-  //тайтл
+app.post(
+  "/videos",
+  (
+    req: Request<
+      {},
+      {},
+      {
+        id: number;
+        title: string;
+        author: string;
+        canBeDownloaded: boolean;
+        minAgeRestriction: number | null;
+        createdAt: string;
+        publicationDate: string;
+        availableResolutions: Array<string>;
+      }
+    >,
+    res: Response
+  ) => {
+    //тайтл
 
-  if (!req.body.title) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Write title",
-          field: "Title",
-        },
-      ],
-    });
-    return;
-  }
-  if (req.body.title.length > 40) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Write title less 40 symbols",
-          field: "Title",
-        },
-      ],
-    });
-    return;
-  }
+    if (!req.body.title) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message: "Write title",
+            field: "Title",
+          },
+        ],
+      });
+      return;
+    }
+    if (req.body.title.length > 40) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message: "Write title less 40 symbols",
+            field: "Title",
+          },
+        ],
+      });
+      return;
+    }
 
-  //автор
+    //автор
 
-  if (req.body.author.length > 20) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Write author less 20 symbols",
-          field: "Author",
-        },
-      ],
-    });
-    return;
-  }
-  if (!req.body.author) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Write author",
-          field: "Author",
-        },
-      ],
-    });
-    return;
-  }
+    if (req.body.author.length > 20) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message: "Write author less 20 symbols",
+            field: "Author",
+          },
+        ],
+      });
+      return;
+    }
+    if (!req.body.author) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message: "Write author",
+            field: "Author",
+          },
+        ],
+      });
+      return;
+    }
 
-  //айди
+    //айди
 
-  let newId = 0;
-  if (bd.videos.length === 0) {
-    newId = 0;
-  } else if (bd.videos.length === 1) {
-    if (bd.videos[0].id !== 0) {
+    let newId = 0;
+    if (bd.videos.length === 0) {
       newId = 0;
-    } else {
-      newId = 1;
-    }
-  } else {
-    for (let i = 0; i < bd.videos.length; i++) {
-      if (bd.videos[i + 1].id - bd.videos[i].id !== 1) {
-        newId = bd.videos[i].id + 1;
-      }
-    }
-  }
-
-  //canBeDownloaded
-
-  let canDownloaded: boolean;
-  if (!req.body.canBeDownloaded) {
-    canDownloaded = false;
-  } else {
-    canDownloaded = req.body.canBeDownloaded;
-  }
-
-  //minAgeRestriction
-  let ageReg: number | null;
-  if (!req.body.minAgeRestriction) {
-    ageReg = null;
-  } else if (
-    req.body.minAgeRestriction < 1 &&
-    req.body.minAgeRestriction > 18
-  ) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message: "Please write age less than 18 included",
-          field: "minAgeRestriction",
-        },
-      ],
-    });
-    return;
-  } else {
-    ageReg = req.body.minAgeRestriction;
-  }
-  //создание
-  let createDate: string;
-  if (!req.body.createdAt) {
-    createDate = new Date().toISOString();
-  } else {
-    let a = Date.parse(req.body.createdAt);
-    let b = new Date(a);
-    let c = b.toISOString();
-    createDate = c;
-  }
-
-  //публикация
-  let publicDate: string;
-  let v = Date.parse(req.body.publicationDate);
-  if (!req.body.publicationDate || Date.parse(createDate) > v) {
-    var today = new Date(createDate);
-    let nextDate = new Date(today.getTime() + 24 * 60 * 60 * 1000);
-    publicDate = nextDate.toISOString();
-  } else {
-    let n = new Date(v);
-    let l = n.toISOString();
-    publicDate = l;
-  }
-
-  //разрешения
-
-  let razreshenie: Array<string> = [];
-
-  const arrayResolutions = [
-    "P144",
-    "P240",
-    "P360",
-    "P480",
-    "P720",
-    "P1080",
-    "P1440",
-    "P2160",
-  ];
-
-  if (!req.body.availableResolutions) {
-    res.status(400).json({
-      errorsMessages: [
-        {
-          message:
-            "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
-          field: "availableResolutions",
-        },
-      ],
-    });
-    return;
-  } else {
-    for (let i = 0; i < req.body.availableResolutions.length; i++) {
-      if (arrayResolutions.indexOf(req.body.availableResolutions[i]) === -1) {
-        res.status(400).json({
-          errorsMessages: [
-            {
-              message:
-                "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
-              field: "availableResolutions",
-            },
-          ],
-        });
-        return;
+    } else if (bd.videos.length === 1) {
+      if (bd.videos[0].id !== 0) {
+        newId = 0;
       } else {
-        razreshenie = req.body.availableResolutions;
+        newId = 1;
+      }
+    } else {
+      for (let i = 1; i < bd.videos.length; i++) {
+        let indexTwo = i - 1;
+        let elementTwo = bd.videos[indexTwo];
+        let idTwo = elementTwo.id;
+        let elementOne = bd.videos[i];
+        let idOne = elementOne.id;
+        let raznitsaId = idOne - idTwo;
+        if (raznitsaId !== 1) {
+          newId = bd.videos[i].id + 1;
+          break;
+        }
+        if (i === bd.videos.length - 1) {
+          newId = bd.videos[i].id + 1;
+          break;
+        }
       }
     }
+
+    //canBeDownloaded
+
+    let canDownloaded: boolean;
+    if (req.body.canBeDownloaded===true) {
+      canDownloaded = true;
+    } else {
+      canDownloaded = false;
+    }
+
+    //minAgeRestriction
+
+    let ageReg;
+    if (!req.body.minAgeRestriction) {
+      ageReg = null;
+    } else if (
+      req.body.minAgeRestriction < 1 ||
+      req.body.minAgeRestriction > 18
+    ) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message: "Please write age less than 18 included",
+            field: "minAgeRestriction",
+          },
+        ],
+      });
+      return;
+    } else {
+      ageReg = req.body.minAgeRestriction;
+    }
+    //создание
+    let createDate: string;
+    if (!req.body.createdAt) {
+      createDate = new Date().toISOString();
+    } else {
+      let a = Date.parse(req.body.createdAt);
+      let b = new Date(a);
+      let c = b.toISOString();
+      createDate = c;
+    }
+
+    //публикация
+    let publicDate: string;
+    let v = Date.parse(req.body.publicationDate);
+    if (!req.body.publicationDate || Date.parse(createDate) > v) {
+      var today = new Date(createDate);
+      let nextDate = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+      publicDate = nextDate.toISOString();
+    } else {
+      let n = new Date(v);
+      let l = n.toISOString();
+      publicDate = l;
+    }
+
+    //разрешения
+
+    let razreshenie: Array<string> = [];
+
+    const arrayResolutions = [
+      "P144",
+      "P240",
+      "P360",
+      "P480",
+      "P720",
+      "P1080",
+      "P1440",
+      "P2160",
+    ];
+
+    if (!req.body.availableResolutions) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message:
+              "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
+            field: "availableResolutions",
+          },
+        ],
+      });
+      return;
+    } else if (req.body.availableResolutions.length === 0) {
+      res.status(400).json({
+        errorsMessages: [
+          {
+            message:
+              "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
+            field: "availableResolutions",
+          },
+        ],
+      });
+      return;
+    } else {
+      for (let i = 0; i < req.body.availableResolutions.length; i++) {
+        if (arrayResolutions.indexOf(req.body.availableResolutions[i]) === -1) {
+          res.status(400).json({
+            errorsMessages: [
+              {
+                message:
+                  "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
+                field: "availableResolutions",
+              },
+            ],
+          });
+          return;
+        } else {
+          razreshenie = req.body.availableResolutions;
+        }
+      }
+    }
+
+    const createVideo: TypeVidios = {
+      id: newId,
+      title: req.body.title,
+      author: req.body.author,
+      canBeDownloaded: canDownloaded,
+      minAgeRestriction: ageReg,
+      createdAt: createDate,
+      publicationDate: publicDate,
+      availableResolutions: razreshenie,
+    };
+
+    bd.videos.push(createVideo);
+    res.status(201).json(createVideo);
   }
-
-  const createVideo: TypeVidios = {
-    id: newId,
-    title: req.body.title,
-    author: req.body.author,
-    canBeDownloaded: canDownloaded,
-    minAgeRestriction: ageReg,
-    createdAt: createDate,
-    publicationDate: publicDate,
-    availableResolutions: razreshenie,
-  };
-
-  bd.videos.push(createVideo);
-  res.status(201).json(createVideo);
-});
+);
 
 app.get("/videos/:id", (req: Request, res: Response) => {
   let id: number = +req.params.id;
@@ -216,17 +259,142 @@ app.get("/videos/:id", (req: Request, res: Response) => {
   }
 });
 
-app.put("/videos/:id", (req: Request, res: Response) => {
-  
+app.put("/videos/:id", (req: Request<
+  {id:string},
+  {},
+  {
+    id: number;
+    title: string;
+    author: string;
+    canBeDownloaded: boolean;
+    minAgeRestriction: number | null;
+    createdAt: string;
+    publicationDate: string;
+    availableResolutions: Array<string>;
+  }
+>, res: Response) => {
+
   let id: number = +req.params.id;
-  let oneVideo = bd.videos.find((p) => p.id === id);
-  if (oneVideo) {
-    res.status(200).json(oneVideo);
+  let thisVideo = bd.videos.find((p) => p.id === id);
+
+  let dublicatVideo = thisVideo;
+  if (dublicatVideo) {
+    if (req.body.title) {
+      if (req.body.title.length > 40) {
+        res.status(400).json({
+          errorsMessages: [
+            {
+              message: "Write title less 40 symbols",
+              field: "Title",
+            },
+          ],
+        });
+        return;
+      }
+      dublicatVideo.title = req.body.title;
+    }
+    //автор
+    if (req.body.author) {
+      if (req.body.author.length > 20) {
+        res.status(400).json({
+          errorsMessages: [
+            {
+              message: "Write author less 20 symbols",
+              field: "Author",
+            },
+          ],
+        });
+        return;
+      }
+      dublicatVideo.author = req.body.author;
+    }
+    //canDownloaded
+    if (req.body.canBeDownloaded === true||false) {
+      dublicatVideo.canBeDownloaded = req.body.canBeDownloaded;
+    }
+
+    //minAgeRestriction
+    if (req.body.minAgeRestriction!==null) {
+      let ageReg: number;
+      if (
+        req.body.minAgeRestriction > 18||req.body.minAgeRestriction < 1
+      ) {
+        res.status(400).json({
+          errorsMessages: [
+            {
+              message: "Please write age less than 18 included",
+              field: "minAgeRestriction",
+            },
+          ],
+        });
+        return;
+      } else {
+        ageReg = req.body.minAgeRestriction;
+        dublicatVideo.minAgeRestriction = ageReg;
+      }} else {
+      dublicatVideo.minAgeRestriction =null;}
+
+    //публикация
+
+    let publicDate: string;
+    publicDate = new Date().toISOString();
+    dublicatVideo.publicationDate = publicDate;
+
+    //разрешения
+
+    let razreshenie: Array<string> = [];
+
+    const arrayResolutions = [
+      "P144",
+      "P240",
+      "P360",
+      "P480",
+      "P720",
+      "P1080",
+      "P1440",
+      "P2160",
+    ];
+
+    if (req.body.availableResolutions) {
+      if (!req.body.availableResolutions) {
+        res.status(400).json({
+          errorsMessages: [
+            {
+              message:
+                "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
+              field: "availableResolutions",
+            },
+          ],
+        });
+        return;
+      } else {
+        for (let i = 0; i < req.body.availableResolutions.length; i++) {
+          if (
+            arrayResolutions.indexOf(req.body.availableResolutions[i]) === -1
+          ) {
+            res.status(400).json({
+              errorsMessages: [
+                {
+                  message:
+                    "Please write right resolution:[ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]",
+                  field: "availableResolutions",
+                },
+              ],
+            });
+            return;
+          } else {
+            razreshenie = req.body.availableResolutions;
+            dublicatVideo.availableResolutions = razreshenie;
+          }
+        }
+      }
+    }
+    thisVideo = dublicatVideo;
+    res.status(200).json(thisVideo);
   } else {
     res.send(404);
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
